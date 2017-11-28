155251cc07b9d49ff115c64c18a1634f8bd98752
jjsoviet <joncalanio@gmail.com>
2017-11-28 15:19:54 -0600

    [master] Adds initial web styles for the login page.

--- 10ben-portal/client/main.js
+++ 10ben-portal/client/main.js
@@ -12,8 +12,20 @@ SimpleSchema.debug = true;
 //Schemas
 
 //Template Helpers
-Template.applicationLayout.onCreated(function() {
+Template.applicationLayout.onCreated(() => {
   Blaze._allowJavascriptUrls();
+});
+
+Template.login.onCreated(() => {
+  setTimeout(() => {
+    $('.at-form').addClass('login-form flex-column flex-center');
+    $('.at-pwd-form form').addClass('flex-column flex-start');
+    $('.at-form .at-title h3').addClass('login-title');
+    $('.at-form .at-title h3').text('Welcome, Controller.');
+    $('.at-pwd-form form .at-input').addClass('login-field');
+    $('.at-pwd-form form .at-btn').addClass('login-button');
+    $('.at-signup-link').addClass('hidden');
+  }, 0);
 });
 
 Template.stub.events({
