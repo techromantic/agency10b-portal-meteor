4fc21032b5577900554a986c83b6f17887b25887
gingergarlic <msqureshi@uh.edu>
2017-12-07 10:18:58 -0600

    [MASTER] Created filtering using ReactiveVars.

--- 10ben-portal/server/publish.js
+++ 10ben-portal/server/publish.js
@@ -7,6 +7,27 @@ Meteor.publish('allAgents', () => {
   return Agents.find({});
 });
 
+Meteor.publish('allDeveloper', () => {
+    return Agents.find({type:'Developer'});
+
+});
+
+Meteor.publish('allDesigner', () => {
+    return Agents.find({type:'Designer'});
+});
+
+Meteor.publish('allWriter', () => {
+    return Agents.find({type:'Writer'});
+});
+
+Meteor.publish('allAnalyst', () => {
+    return Agents.find({type:'Analyst'});
+});
+
+Meteor.publish('allStrategist', () => {
+    return Agents.find({type:'Strategist'});
+});
+
 Meteor.publish('allAssignments', () => {
   return Assignments.find({});
 });
